6f94b2bd461cb1658da9400d90ee5a97d3b2139e
KhoaTran235 <136189631+KhoaTran235@users.noreply.github.com>
2024-05-09 23:48:12 +0700

    Add files via upload

---
+++ face.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from keras.models import load_model  # TensorFlow is required for Keras to work (version == 2.13.0)
+import cv2  # Install opencv-python
+import numpy as np
+from scipy import spatial
+
+# Load the embedding_layer
+embedding_layer = load_model("embedding_layer.h5", compile=False)
+
+# Embedding model
+anchor_input = tf.keras.Input(shape=(100,75,3))
+anchor_embeddings = embedding_layer(anchor_input)
+embedding_model = models.Model(inputs=anchor_input, outputs=anchor_embeddings)
+
+# Face detect with Haar Cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+pic = cv2.imread('./Khoa1.jpg') #modify with cv2 capture image
+gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+face = face_cascade.detectMultiScale(gray, 1.1, 4)
+face = face[np.argmax(face[:, 2])]
+face = np.expand_dims(face, 0)
+for (x, y, w, h) in face:
+    face = pic[y:y + h, x:x + w]
+    face = cv2.resize(face, (75, 100))
+    face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+
+embedding = embedding_model.predict(np.expand_dims(np.array(face), axis = 0))
+
+s = ""
+with open('./image_features.txt', "r") as file:
+    for line in file:
+        s += line
+
+def recognition(s, embedding, threshold):
+    for i in range(s.count(',')):
+        string = s.split(',')[i]
+        class_name = string[:string.index(' ')].replace("\n", "")
+        vector = np.fromstring(string[string.index(' ')+3:string.index(']]')].replace("\n", ""), dtype=float, sep=' ')
+        similarity = 1 - spatial.distance.cosine(embedding[0], vector)
+        print(similarity)
+        if similarity >= threshold:
+            return class_name, True
+    return "Unknown", False
+
+threshold = 0.9
+actor, isValid = recognition(s, embedding, threshold)
+print(actor)
